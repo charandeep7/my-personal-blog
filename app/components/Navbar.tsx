ddafaf61f57b8998213c80084937db812809db3b
kitish <kitishkumar2003@gmail.com>
2023-07-18 02:05:25 +0530

    Made a blog app using next13 version 1.0

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import ThemeButton from "./ThemeButton";
+
+export default function Navbar() {
+  return (
+    <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-16">
+            <div className="flex justify-between item-center w-full">
+                <Link href='/'>
+                    <h1 className="text-2xl font-medium">
+                        Kitish <span className="text-teal-500">
+                            Blog
+                        </span>
+                    </h1>
+                </Link>
+                <ThemeButton />
+            </div>
+        </div>
+    </div>
+  )
+}
---
+++ types.d.ts
@@ -0,0 +1,10 @@
+type Post = {
+    title: string,
+    overview: string,
+    content: any,
+    _id: string,
+    slug: {
+        current: string,
+    },
+    _createdAt: string
+}
